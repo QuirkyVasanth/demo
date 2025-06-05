09286c18bf1f452252e454ca5b63149ed7da4c70
QuirkyVasanth <vasanthneiva@gmail.com>
2025-06-05 11:58:02 +0530

    Day 1 Completed

---
+++ Day_1_Task/Task1.java
@@ -0,0 +1,9 @@
+package addon_DSA;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My task 1 Executed");
+	}
+}
---
+++ PostAssignment_Day1/PostAssignment1.java
@@ -0,0 +1,8 @@
+package addon_DSA;
+
+public class PostAssignment1 {
+
+	public static void main(String[] args) {
+		System.out.println("Post Assignent Executed");
+	}
+}
